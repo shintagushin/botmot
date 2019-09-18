e1f2d85dd4c71b6386b17111574be6c21e17192b
unknown <taguchs@alpha.co.jp>
2019-09-18 13:09:41 +0900

    新規作成

---
+++ Botmot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace botmot
+{
+    public class Botmot : IBot
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            // ユーザーのメッセージにだけ返信
+            // Text プロパティの値をオウム返し
+            if (turnContext.Activity.Type == ActivityTypes.Message
+                && !string.IsNullOrEmpty(turnContext.Activity.Text))
+                await turnContext.SendActivityAsync(turnContext.Activity.Text);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.5.0
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using botmot.Bots;
+using Microsoft.AspNetCore.Http;
+
+namespace botmot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBot<Botmot>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseBotFramework();
+        }
+    }
+}
